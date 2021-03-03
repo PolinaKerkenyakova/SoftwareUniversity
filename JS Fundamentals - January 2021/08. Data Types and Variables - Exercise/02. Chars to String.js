731cbf58120d60825622bbe7d94a321427abec32
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-03 14:27:15 +0200

    Delete files

--- JS Fundamentals - January 2021/08. Data Types and Variables - Exercise/02. Chars to String.js
+++
@@ -1,8 +0,0 @@
-function charsToString(firstInput, secondInput, thirdInput) {
-
-    console.log(`${firstInput}${secondInput}${thirdInput}`);
-}
-
-charsToString('a', 'b', 'c');
-charsToString('%', '2', 'o');
-charsToString('1', '5', 'p');
--- JS Fundamentals - January 2021/08. Data Types and Variables - Exercise/Data Types and Variables - More Exercise/04. Biggest of 3 Numbers.js
+++
@@ -1,8 +0,0 @@
-function biggestNumber(fisrtNumber, secondNumber, thirdNumber) {
-
-    console.log(Math.max(fisrtNumber, secondNumber, thirdNumber));
-}
-
-biggestNumber(-2, 7, 3);
-biggestNumber(130, 5, 99);
-biggestNumber(43, 43.2, 43.1);
