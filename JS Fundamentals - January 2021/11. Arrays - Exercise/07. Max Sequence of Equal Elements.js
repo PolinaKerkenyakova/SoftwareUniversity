5c8770caded2946c09c2ee711c02c62f74da7532
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-03 17:27:04 +0200

    Solution update

--- JS Fundamentals - January 2021/11. Arrays - Exercise/07. Max Sequence of Equal Elements.js
+++ JS Fundamentals - January 2021/11. Arrays - Exercise/07. Max Sequence of Equal Elements.js
@@ -26,6 +26,7 @@ function maxSequence(numArray) {
 }
 
 maxSequence([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
-// maxSequence([1, 1, 1, 2, 3, 1, 3, 3]);
-// maxSequence([4, 4, 4, 4]);
-// maxSequence([0, 1, 1, 5, 2, 2, 6, 3, 3]);
+maxSequence([1, 1, 1, 2, 3, 1, 3, 3]);
+maxSequence([4, 4, 4, 4]);
+maxSequence([0, 1, 1, 5, 2, 2, 6, 3, 3]);
+
