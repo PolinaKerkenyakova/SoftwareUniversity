9d4bb08d812d75f8397d9c896143ea4aa21dab79
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-04 11:27:51 +0200

    Solution update

--- JS Fundamentals - January 2021/11. Arrays - Exercise/Arrays - More Exercise/04. Non-Decreasing Subsequence.js
+++ JS Fundamentals - January 2021/11. Arrays - Exercise/Arrays - More Exercise/04. Non-Decreasing Subsequence.js
@@ -1,16 +1,6 @@
 function notDecreasingSequance(numberArray) {
 
-    let biggerNumber = 0;
-    let modifiedArray = [];
-
-    for (let i = 0; i < numberArray.length; i++) {
-        if (biggerNumber < numberArray[i]) {
-            biggerNumber = numberArray[i];
-        }
-        
-        modifiedArray.push(biggerNumber);
-
-    }
+    
 }
 
 notDecreasingSequance([ 1, 3, 8, 4, 10, 12, 3, 2, 24]);
