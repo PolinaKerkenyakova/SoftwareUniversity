9e739cd414d0d264cc6ff752b41077f9785eb880
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-08 09:43:26 +0200

    Solution update

--- JS Fundamentals - January 2021/20. Objects and Classes - Exercise/02. Towns.js
+++ JS Fundamentals - January 2021/20. Objects and Classes - Exercise/02. Towns.js
@@ -16,3 +16,27 @@ function towns(input) {
 
 towns(['Sofia | 42.696552 | 23.32601',
 'Beijing | 39.913818 | 116.363625']);
+
+
+// function towns(input) {
+
+//     let towns = [];
+//    for (let data of input) {
+//        let [town, latitude, longitude] = data.split(' | ');
+
+//        town = {
+//            town,
+//            latitude: Number(latitude).toFixed(2),
+//            longitude: Number(longitude).toFixed(2),
+//        }
+
+//        towns.push(town);
+//    }
+
+//    for (let town of towns) {
+//        console.log(town)
+//    }
+// }
+
+// towns(['Sofia | 42.696552 | 23.32601',
+// 'Beijing | 39.913818 | 116.363625']);
