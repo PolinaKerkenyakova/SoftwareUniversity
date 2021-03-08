c87cecf20a64b3fb7573e3ff05c125026a90087b
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-08 11:55:06 +0200

    Solution update

--- JS Fundamentals - January 2021/20. Objects and Classes - Exercise/04. Movies.js
+++ JS Fundamentals - January 2021/20. Objects and Classes - Exercise/04. Movies.js
@@ -1,5 +1,48 @@
 function movies(input) {
 
+    let movies = [];
+
+    for (let i = 0; i < input.length; i++) {
+
+        let tokens = input[i].split(' ');
+        let addMovieIndex = tokens.indexOf('addMovie');
+        let directedByIndex = tokens.indexOf('directedBy');
+        let onDateIndex = tokens.indexOf('onDate');
+
+        if (addMovieIndex > -1) {
+            let name = tokens.slice(addMovieIndex + 1).join(' ');
+            movies.push({ name });
+        }
+
+        if (directedByIndex > -1) {
+            let name = tokens.slice(0, directedByIndex).join(' ');
+            let director = tokens.slice(directedByIndex + 1).join(' ');
+
+            movies.forEach(movie => {
+                if (movie.name === name) {
+                    movie.director = director;
+                }
+            });
+        }
+
+        if (onDateIndex > -1) {
+            let name = tokens.slice(0, onDateIndex).join(' ');
+            let date = tokens.slice(onDateIndex + 1).join('.');
+
+            movies.forEach(movie => {
+
+                if (movie.name === name) {
+                    movie.date = date;
+                }
+            });
+        }
+    }
+
+    movies.forEach(movie => {
+        if (movie.name !== undefined && movie.date !== undefined && movie.director !== undefined) {
+            console.log(JSON.stringify(movie));
+        }
+    });
 }
 
 movies([
@@ -11,4 +54,4 @@ movies([
     'Fast and Furious onDate 30.07.2018',
     'Batman onDate 01.08.2018',
     'Fast and Furious directedBy Rob Cohen'
-    ]);
+]);
