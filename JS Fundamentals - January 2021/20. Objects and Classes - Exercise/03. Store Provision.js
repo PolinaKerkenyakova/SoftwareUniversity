5165fb2744b2c916b50e0f9a9559029b57e22a60
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-08 10:15:04 +0200

    Solution Update

--- JS Fundamentals - January 2021/20. Objects and Classes - Exercise/03. Store Provision.js
+++ JS Fundamentals - January 2021/20. Objects and Classes - Exercise/03. Store Provision.js
@@ -1,31 +1,35 @@
 function storeProvision(stock, ordered) {
 
-    const products = {};
+    let products = {};
 
-    for (let i = 0; i < stock.length; i += 2) {
-        let item = stock[i];
+    for (let i = 0; i < stock.length; i+=2) {
+        let product = stock[i];
         let quantity = Number(stock[i + 1]);
 
-        products[item] = quantity;
+        products[product] = quantity;
     }
 
-    for (let i = 0; i < ordered.length; i += 2) {
-        let item = ordered[i];
+    for (let i = 0; i < ordered.length; i+=2) {
+        let product = ordered[i];
         let quantity = Number(ordered[i + 1]);
 
-        if (products[item] === undefined) {
-            products[item] = 0;
+        let allProducts = Object.keys(products);
+
+        if (allProducts.includes(product)) {
+            products[product] += quantity;
+        } else {
+            products[product] = quantity;
         }
-        products[item] += quantity;
     }
 
     for (let product in products) {
         console.log(`${product} -> ${products[product]}`)
     }
 }
+
 storeProvision([
     'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
-],
+    ],
     [
-        'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
+    'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
     ]);
