4b74200dc6a8d8a02f44ebb5d5090782c8edf8f4
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-16 14:26:44 +0200

    Solution update

---
+++ JS Fundamentals - January 2021/31. Regular Expresions - Exercise/01. Furniture.js
@@ -0,0 +1,22 @@
+function furniture(input) {
+
+    let boughtFurniture = [];
+    let totalMoneySpend = 0;
+
+    for (let data of input) {
+        let match = />>(?<product>[A-Za-z]+)<<(?<price>[\d\.*\d*]+)\!(?<quantity>[\d]+)/g.exec(data);
+
+        if (match) {
+            boughtFurniture.push(match.groups.product)
+            totalMoneySpend += Number(match.groups.price) * Number(match.groups.quantity);
+        }
+    }
+
+    console.log('Bought furniture:');
+    if(boughtFurniture.length > 0) {
+        console.log(boughtFurniture.join('\n'))
+    }
+    console.log(`Total money spend: ${totalMoneySpend.toFixed(2)}`);
+}
+
+furniture(['>>Sofa<<312.23!3', '>>TV<<300!5', '>Invalid<<!5', 'Purchase']);
