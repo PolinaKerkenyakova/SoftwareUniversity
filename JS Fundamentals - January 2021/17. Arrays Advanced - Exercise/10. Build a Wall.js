9bbea1599e349a635cc73323280f29e08aad8953
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-20 14:32:06 +0200

    Solution update

--- JS Fundamentals - January 2021/17. Arrays Advanced - Exercise/10. Build a Wall.js
+++ JS Fundamentals - January 2021/17. Arrays Advanced - Exercise/10. Build a Wall.js
@@ -1,5 +1,33 @@
-function buildWall(input) {
+function buildWall(inputArr) {
 
+    let input = inputArr.map(Number);
+    let dailyConcrete = [];
+    let totalConcrete = 0;
+    let cost = 0;
+    let crews = input.filter(len => len < 30).length;
+
+    while (crews !== 0) {
+        let concreteByAllCrews = 0;
+
+        for (let i = 0; i < input.length; i++) {
+            if (input[i] !== 30) {
+                input[i]++;
+                concreteByAllCrews += 195;
+
+                if (input[i] === 30){
+                    crews--;
+                }
+            }
+        }
+
+        totalConcrete += concreteByAllCrews;
+        dailyConcrete.push(concreteByAllCrews);
+    }
+
+    cost = totalConcrete * 1900;
+    
+    console.log(dailyConcrete. join(', '));
+    console.log(`${cost} pesos`);
 }
 
 buildWall([21, 25, 28]);
