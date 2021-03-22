ccc91b87f96fb97cc93f6725437bb3352106e94c
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-22 12:47:17 +0200

    Solution update

--- JS Fundamentals - January 2021/I. Mid Exams/02. Programming Fundamentals Mid Exam/01. SoftUni Reception.js
+++ JS Fundamentals - January 2021/I. Mid Exams/02. Programming Fundamentals Mid Exam/01. SoftUni Reception.js
@@ -1,5 +1,25 @@
 function softuniReception(inputArr) {
 
+    let input = inputArr.map(Number);
+
+    let firstEmployee = input[0];
+    let secondEmployee = input[1];
+    let thirdEmployee = input[2];
+
+    let helpedStdentsPerHour = firstEmployee + secondEmployee + thirdEmployee;
+
+    let studentsCount = input[3];
+
+    let hours = 0;
+    while(studentsCount > 0) {
+        hours++;
+
+        if(!(hours % 4 === 0)) {
+            studentsCount -= helpedStdentsPerHour;
+        }
+    }
+
+    console.log(`Time needed: ${hours}h.`)
 }
 
 softuniReception(['5','6','4','20']);
---
+++ JS Fundamentals - January 2021/I. Mid Exams/02. Programming Fundamentals Mid Exam/02. Array Modifier.js
@@ -0,0 +1,52 @@
+function arrayModifier(inputArr) {
+
+
+    let numbers = inputArr.shift().split(' ').map(Number);
+
+    for(let i = 0; i < inputArr.length; i++) {
+        let [command, indexOne, indexTwo] = inputArr[i].split(' ');
+
+        indexOne = Number(indexOne);
+        indexTwo = Number(indexTwo);
+
+        if (command === 'swap') {
+           let numOnINdexOne = numbers[indexOne];
+           let numOnIndexTwo = numbers[indexTwo];
+        
+           numbers[indexOne] = numOnIndexTwo;
+           numbers[indexTwo] = numOnINdexOne;
+
+        } else if (command === 'multiply') {
+            let num = numbers[indexOne] * numbers[indexTwo];
+            numbers.splice(indexOne, 1, num);
+
+        } else if (command === 'decrease') {
+            numbers = numbers.map(x => x - 1);
+        } else if (command === 'end') {
+            console.log(numbers.join(', '));
+            break;
+        }
+    }
+}
+
+arrayModifier
+([
+    '23 -2 321 87 42 90 -123',
+    'swap 1 3',
+    'swap 3 6',
+    'swap 1 0',
+    'multiply 1 2',
+    'multiply 2 1',
+    'decrease',
+    'end'
+  ]);
+//   arrayModifier
+//   ([
+//     '1 2 3 4',
+//     'swap 0 1',
+//     'swap 1 2',
+//     'swap 2 3',
+//     'multiply 1 2',
+//     'decrease',
+//     'end'
+//   ]);
