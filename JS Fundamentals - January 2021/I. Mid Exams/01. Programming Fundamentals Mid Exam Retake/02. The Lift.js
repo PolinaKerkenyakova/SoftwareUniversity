14e9808d2773913ac47d0d36ab4bdca69deb2a8f
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-23 10:20:30 +0200

    Solution update

--- JS Fundamentals - January 2021/I. Mid Exams/01. Programming Fundamentals Mid Exam Retake/02. The Lift.js
+++ JS Fundamentals - January 2021/I. Mid Exams/01. Programming Fundamentals Mid Exam Retake/02. The Lift.js
@@ -1,19 +1,53 @@
 function theLift(inputArr) {
 
     let peopleWaiting = Number(inputArr[0]);
+    let people = peopleWaiting;
     let wagons = inputArr[1].split(' ').map(Number);
 
-    for (let i = 0; i < wagons.length; i++) {
-        
+    let allEmptySport = 0;
+
+    for (let seat of wagons) {
+        allEmptySport += 4 - seat;
     }
 
+    for (let i = 0; i < wagons.length; i++) {
+        if (4 - wagons[i] > 0) {
+            let seats = 4 - wagons[i];
+            if (peopleWaiting < 4) {
+                seats = peopleWaiting;
+            }
+            wagons[i] += seats;
+            peopleWaiting -= seats;
+        }
+    }
+
+
+    let areThereEmptySports = false;
+    for (let seat of wagons) {
+        if (seat < 4) {
+            areThereEmptySports = true;
+            break;
+        }
+    }
+
+    if (allEmptySport === people && areThereEmptySports === false) {
+        console.log(wagons.join(' '));
+
+    } else if (areThereEmptySports) {
+        console.log(`The lift has empty spots!`);
+        console.log(wagons.join(' '));
+
+    } else if (peopleWaiting > 0 && areThereEmptySports === false) {
+        console.log(`There isn't enough space! ${peopleWaiting} people in a queue!`);
+        console.log(wagons.join(' '))
+    }
 }
 
-theLift
-    ([
-        "15",
-        "0 0 0 0 0"
-    ]);
+// theLift
+//     ([
+//         "15",
+//         "0 0 0 0 0"
+//     ]);
 theLift
     ([
         "20",
