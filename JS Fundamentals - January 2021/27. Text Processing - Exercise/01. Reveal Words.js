d57fbbe629042578d63166a2f096037d5bccf7d6
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-03-29 11:42:54 +0300

    Solution update

--- JS Fundamentals - January 2021/27. Text Processing - Exercise/01. Reveal Words.js
+++ JS Fundamentals - January 2021/27. Text Processing - Exercise/01. Reveal Words.js
@@ -13,5 +13,5 @@ function revealWords(string, text) {
 
 revealWords('great',
     'softuni is ***** place for learning new programming languages');
-// revealWords('great, learning',
-// 'softuni is ***** place for ******** new programming languages');
+revealWords('great, learning',
+'softuni is ***** place for ******** new programming languages');
