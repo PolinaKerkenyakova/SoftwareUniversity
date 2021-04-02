fdffa2305d8cff31cc32f3ba49d95c989a6d4e86
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-04-02 16:29:11 +0300

    Solution update

--- JS Fundamentals - January 2021/II. Final Exams/05. Programming Fundamentals Final Exam/01. Activation Keys.js
+++ JS Fundamentals - January 2021/II. Final Exams/05. Programming Fundamentals Final Exam/01. Activation Keys.js
@@ -1,54 +1,51 @@
 function activationKeys(inputArr) {
-
     let input = [...inputArr];
-    
+
     let key = input.shift();
-    // let rawActivationKey = key;
 
-    let command = input.shift();
+    let line = input.shift();
+    while (line !== 'Generate') {
 
-    while (command !== 'Generate') {
+        let info = line.split('>>>');
 
-        let [operation, strOne, strTwo, strThree] = command.split('>>>')
-
-        if (operation === 'Contains') {
-
-            let str = strOne;
-
-            if (key.indexOf(str) > -1) {
-                console.log(`${key} contains ${srt}`);
+        let command = info[0];
+        if (command === 'Contains') {
+            let substring = info[1];
+            if (key.includes(substring)) {
+                console.log(`${key} contains ${substring}`)
             } else {
                 console.log(`Substring not found!`);
             }
-        } else if (operation === 'Flip') {
-            let letterCase = strOne;
-            let startIndex = Number(strTwo);
-            let endIndex = Number(strThree);
+        } else if (command === 'Flip') {
+            let letterCase = info[1];
+            let startIndex = Number(info[2]);
+            let endIndex = Number(info[3]);
 
-            key = key.split('');
-            for (let i = startIndex; i < endIndex; i++) {
-                if (letterCase === 'Upper') {
-                    key[i] = key[i].toUpperCase();
-                } else if (letterCase === 'Lower') {
-                    key[i] = key[i].toLowerCase();
-                }
+            let subStr = key.substring(startIndex, endIndex);
+            let flippedSubStr = '';
+
+            if (letterCase === 'Upper') {
+                flippedSubStr = subStr.toUpperCase();
+            } else {
+                flippedSubStr = subStr.toLowerCase();
             }
 
-            key = key.join('');
+            key = key.replace(subStr, flippedSubStr);
             console.log(key);
-        } else if (operation === 'Slice') {
-            let startIndex = Number(strOne);
-            let endIndex = Number(strTwo);
+        } else if (command === 'Slice') {
+            let startIndex = Number(info[1]);
+            let endIndex = Number(info[2]);
 
-            key = key.split('');
-            key.splice(startIndex, endIndex - startIndex);
-            key = key.join('');
-            console.log(key);
+            let subStr = key.substring(startIndex, endIndex);
+
+            key = key.replace(subStr, '');
+            console.log(key)
         }
-        command = input.shift();
+
+        line = input.shift();
     }
 
-    console.log(`Your activation key is: ${key}`);
+    console.log(`Your activation key is: ${key}`)
 }
 
 // activationKeys([
@@ -61,7 +58,7 @@ function activationKeys(inputArr) {
 //     'Generate'
 //   ]);
 
-  activationKeys([
+activationKeys([
     '134softsf5ftuni2020rockz42',
     'Slice>>>3>>>7',
     'Contains>>>-rock',
@@ -70,4 +67,4 @@ function activationKeys(inputArr) {
     'Flip>>>Upper>>>2>>>8',
     'Flip>>>Lower>>>5>>>11',
     'Generate'
-  ]);
+]);
