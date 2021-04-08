7b004a4852cfd72f28519a8ca66a7aeeb2091ffd
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-04-08 15:56:58 +0300

    Solution update

--- JS Fundamentals - January 2021/II. Final Exams/02. Programming Fundamentals Final Exam/03. Plant Discovery.js
+++ JS Fundamentals - January 2021/II. Final Exams/02. Programming Fundamentals Final Exam/03. Plant Discovery.js
@@ -1,83 +1,59 @@
 function plantDiscovery(input) {
-
     let plantCount = Number(input.shift());
-
-    let plants = {};
-    
-
+    let plantCollection = {};
+ 
     for (let i = 0; i < plantCount; i++) {
-        let [plant, rarity] = input.shift().split('<->');
-
+        let line = input.shift();
+        let [plant, rarity] = line.split('<->');
         rarity = Number(rarity);
-        plants[plant] = { rarity };
+        plantCollection[plant] = { rarity, rating: [] };
     }
-
-    let commandLine = input.shift();
-    while (commandLine !== 'Exhibition') {
-        let counter = 0;
-        let [command, info] = commandLine.split(': ');
-        let [plant, firstArgument] = info.split(' - ');
-
-        if (command === 'Rate') {
-            let rating = Number(firstArgument);
-            if (plants.hasOwnProperty(plant)) {
-                if (plants[plant].rating > 0) {
-                    plants[plant].rating += rating;
-                } else {
-                    plants[plant].rating = rating;
+ 
+    let line = input.shift();
+    while (line !== 'Exhibition') {
+        let [command, ...rest] = line.split(': ');
+        let [plant, data] = rest[0].split(' - ');
+        if (plantCollection.hasOwnProperty(plant)) {
+            switch (command) {
+                case 'Rate': {
+                    let rating = Number(data);
+                    plantCollection[plant].rating.push(rating);
+                    break;
                 }
-
-                counter++
-                if (plants[plant].counter > 0) {
-                    plants[plant].counter += counter;
-
-                } else {
-                    plants[plant].counter = counter;
+                case 'Update': {
+                    let newRarity = Number(data);
+                    plantCollection[plant].rarity = newRarity;
+                    break;
                 }
-
-            } else {
-                console.log('error')
+                case 'Reset': {
+                    plantCollection[plant].rating = [];
+                    break;
+                }
+                default:
+                    console.log('error');
+                    break;
             }
-
-        } else if (command === 'Update') {
-            let rarity = Number(firstArgument)
-            if (plants.hasOwnProperty(plant)) {
-                plants[plant].rarity = rarity;
-            } else {
-                console.log('error')
-            }
-        } else if (command === 'Reset') {
-            if (plants.hasOwnProperty(plant)) {
-                plants[plant].rating = 0;
-                plants[plant].counter = 0;
-            } else {
-                console.log('error')
-            }
+        } else {
+            console.log('error');
         }
-
-        commandLine = input.shift();
+        line = input.shift();
     }
-
-    let plantsEntries = Object.entries(plants);
-
-    plantsEntries.sort((a, b) => {
-        if (a[1].rarity === b[1].rarity) {
-            let counterA = a[1].counter;
-            let counterB = b[1].counter;
-            return (b[1].rating / counterA) - (a[1].rating / counterB)
-        } else {
-            return b[1].rarity - a[1].rarity;
-        }
-    });
-
-    console.log(`Plants for the exhibition:`)
-    for (const kvp of plantsEntries) {
-        if (kvp[1].rating === 0) {
-            console.log(`- ${kvp[0]}; Rarity: ${kvp[1].rarity}; Rating: ${(kvp[1].rating).toFixed(2)}`)
-        } else {
-            console.log(`- ${kvp[0]}; Rarity: ${kvp[1].rarity}; Rating: ${(kvp[1].rating/kvp[1].counter).toFixed(2)}`)
-        }
-        
+ 
+    let sortedPlants = Object.keys(plantCollection).sort(
+        (a, b) =>
+            plantCollection[b].rarity - plantCollection[a].rarity ||
+            average(plantCollection[b].rating) - average(plantCollection[a].rating)
+    );
+ 
+    console.log(`Plants for the exhibition:`);
+ 
+    for (let plant of sortedPlants) {
+        console.log(`- ${plant}; Rarity: ${plantCollection[plant].rarity}; Rating: ${average(plantCollection[plant].rating).toFixed(2)}`);
+    }
+ 
+    function average(arr) {
+        if (!arr.length) return 0;
+        return arr.reduce((a, b) => a + b, 0) / arr.length;
     }
 }
 
@@ -95,11 +71,11 @@ plantDiscovery([
     'Exhibition'
 ]);
 
-// plantDiscovery([
-//     '2',
-//     'Candelabra<->10',
-//     'Oahu<->10',
-//     'Rate: Oahu - 7',
-//     'Rate: Candelabra - 6',
-//     'Exhibition'
-// ]);
+plantDiscovery([
+    '2',
+    'Candelabra<->10',
+    'Oahu<->10',
+    'Rate: Oahu - 7',
+    'Rate: Candelabra - 6',
+    'Exhibition'
+]);
