84c80ad57f5311815288b32d999a5bcdde7239a3
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-05-21 11:01:51 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/04. Syntax, Functions and Statements - Exercise/08. Words Uppercase.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/04. Syntax, Functions and Statements - Exercise/08. Words Uppercase.js
@@ -1,20 +1,15 @@
 function wordsUpperCase(input) {
 
-    let regex = /[A-Za-z]+/g;
+    let regex = /[\w]+/g;
 
     let foundMatches = input.match(regex);
 
     let upperCaseMatches = [];
 
-    for (let word of foundMatches) {
-        word = word.toUpperCase();
-        upperCaseMatches.push(word);
-    }
-
-    // foundMatches.forEach(word => {
-    //     word = word.toUpperCase();
-    //     upperCaseMatches.push(word)
-    // });
+    foundMatches.forEach(word => {
+            word = word.toUpperCase();
+            upperCaseMatches.push(word);
+    });
 
     console.log(upperCaseMatches.join(', '));
 }
