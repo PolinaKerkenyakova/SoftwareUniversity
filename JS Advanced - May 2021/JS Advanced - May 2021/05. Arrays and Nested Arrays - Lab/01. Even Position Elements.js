cb29bb29d86e42557c6c7af3766387567be4c60a
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-05-22 13:15:23 +0300

    Solution Update

---
+++ JS Advanced - May 2021/JS Advanced - May 2021/05. Arrays and Nested Arrays - Lab/01. Even Position Elements.js
@@ -0,0 +1,14 @@
+function evenPositionElements(inputArray) {
+
+    let elements = [];
+
+    for (let i = 0; i < inputArray.length; i+=2) {
+       let element = inputArray[i];
+        elements.push(element);
+    }
+
+    console.log(elements.join(' '));
+}
+
+evenPositionElements(['20', '30', '40', '50', '60']);
+evenPositionElements(['5', '10']);
