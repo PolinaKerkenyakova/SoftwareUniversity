1017d128bcfa9cd348977d3d56c536def9ec0340
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-05-22 13:30:10 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/05. Arrays and Nested Arrays - Lab/04. Negative Or Positive Numbers.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/05. Arrays and Nested Arrays - Lab/04. Negative Or Positive Numbers.js
@@ -1,1 +1,17 @@
-function negativeOrPositiveNumbers(inputArray)
+function negativeOrPositiveNumbers(inputArray) {
+
+    let modifiedArray = [];
+    for (let num of inputArray) {
+
+        if (num < 0) {
+            modifiedArray.unshift(num)
+        } else {
+            modifiedArray.push(num)
+        }
+    }
+
+    console.log(modifiedArray.join('\n'));
+}
+
+negativeOrPositiveNumbers([7, -2, 8, 9]);
+negativeOrPositiveNumbers([3, -2, 0, -1]);
