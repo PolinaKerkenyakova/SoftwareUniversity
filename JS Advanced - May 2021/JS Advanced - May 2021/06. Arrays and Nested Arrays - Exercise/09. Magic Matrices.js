26895fd1331f2d3bf2d0a41c672c34234ab139a9
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-05-28 13:05:59 +0300

    Solution Update

---
+++ JS Advanced - May 2021/JS Advanced - May 2021/06. Arrays and Nested Arrays - Exercise/09. Magic Matrices.js
@@ -0,0 +1,60 @@
+function magicMatrices(array) {
+
+    let sumOfRows = 0;
+    array.forEach((row, index) => {
+        let isEqual = true;
+        if (array[index + 1]) {
+            if (
+                row.reduce((acc, el) => acc + el) ==
+                array[index + 1].reduce((acc, el) => acc + el)
+            ) {
+                sumOfRows = row.reduce((acc, el) => acc + el);
+            } else {
+                isEqual = false;
+            }
+        }
+    });
+
+    let counter = 0;
+    let sumOfColumns = 0;
+    let currSum = 0;
+    let fistCHeck = false;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            sumOfColumns += array[j][counter];
+        }
+        if (currSum !== sumOfColumns && !fistCHeck) {
+            currSum = sumOfColumns;
+            fistCHeck = true;
+        } else {
+            fistCHeck = false;
+            break;
+        }
+        sumOfColumns = 0;
+        counter++;
+    }
+
+    if (sumOfColumns === sumOfRows) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+magicMatrices([
+    [4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]
+]);
+
+magicMatrices([
+    [11, 32, 45],
+    [21, 0, 1],
+    [21, 1, 1]
+]);
+
+magicMatrices([
+    [1, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0]
+]);
