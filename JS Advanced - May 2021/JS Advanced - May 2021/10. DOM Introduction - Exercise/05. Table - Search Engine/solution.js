22b61d15d52c2e60d1c4c5a58ee9a206a243403c
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-02 18:21:04 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,18 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let trElements = document.querySelectorAll('tbody tr');
+      let input = document.querySelector('#searchField');
+      let inputValue = input.value;
 
+      for (let tr of trElements) {
+         if (tr.textContent.includes(inputValue)) {
+            tr.classList.add("select");
+         } else {
+            tr.classList.remove("select");
+         }
+      }
+
+      input.value = '';
    }
 }
