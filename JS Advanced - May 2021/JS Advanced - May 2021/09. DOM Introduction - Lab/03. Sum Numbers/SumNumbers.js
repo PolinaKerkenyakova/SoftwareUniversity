5236e8b345734d61c7901edba951f366aafe3696
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-02 11:28:36 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/09. DOM Introduction - Lab/03. Sum Numbers/SumNumbers.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/09. DOM Introduction - Lab/03. Sum Numbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let firstNum = document.querySelector('#num1').value;
+    let secondNum = document.querySelector('#num2').value;
+    let sumField = document.querySelector('#sum');
+
+    sumField.value = Number(firstNum) + Number(secondNum);
 }
