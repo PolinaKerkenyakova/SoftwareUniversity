7312eea8e28bd3824077b7c293c8f947389baa5d
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-02 10:54:02 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/09. DOM Introduction - Lab/01. Edit Element/EditElement.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/09. DOM Introduction - Lab/01. Edit Element/EditElement.js
@@ -1,3 +1,6 @@
-function editElement() {
-    // TODO
+function editElement(elementReference, match, replacer) {
+    let content = elementReference.textContent;
+    let matcher = new RegExp(match, 'g');
+    let editedContent = content.replace(matcher, replacer);
+    elementReference.textContent = editedContent;
 }
