0e390f162adbacc658d21f1a47ff80b7f8b6456d
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-02 14:48:10 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/09. DOM Introduction - Lab/06. Sum Table/SumTable.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/09. DOM Introduction - Lab/06. Sum Table/SumTable.js
@@ -1,3 +1,14 @@
 function sumTable() {
 
+    let tr = document.querySelectorAll('tr');
+    let sum = 0;
+
+    for (let i = 1; i < tr.length - 1; i++) {
+        let currentTr = tr[i];
+        let tdCost = currentTr.children[1].textContent;
+        sum += Number(tdCost);
+    }
+
+    let totalSum = document.querySelector('#sum');
+    totalSum.textContent = sum;
 }
