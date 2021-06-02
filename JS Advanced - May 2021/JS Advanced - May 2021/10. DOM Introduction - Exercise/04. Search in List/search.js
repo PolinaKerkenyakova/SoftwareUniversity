c28dea8b5c553064862de06a2e31e9bbc04b8693
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-02 17:37:02 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/04. Search in List/search.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+
+   let liElements = document.querySelectorAll('li');
+   let input = document.querySelector('#searchText').value;
+   let result = document.querySelector('#result');
+
+   let matchesFound = 0;
+
+   for (const li of liElements) {
+      if (li.textContent.includes(input)) {
+         li.style.fontWeight = 'bold';
+         li.style.textDecoration = 'underline';
+         matchesFound++
+      } else {
+         li.style.fontWeight = '';
+         li.style.textDecoration = '';
+      }
+   }
+
+   result.textContent = `${matchesFound} matches found`
 }
