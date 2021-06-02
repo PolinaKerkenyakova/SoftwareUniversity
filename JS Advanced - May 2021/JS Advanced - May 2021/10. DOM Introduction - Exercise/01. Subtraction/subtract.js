aaf33413de24df3df95e91d212ec83f6e006e232
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-02 14:58:54 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/01. Subtraction/subtract.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,9 @@
-function subtract() {
-    console.log('TODO:...');
+function subtract(firstNumber, secondNumber) {
+
+    firstNumber = Number(document.querySelector('#firstNumber').value);
+    secondNumber = Number(document.querySelector('#secondNumber').value);
+
+    let dispayDiv = document.querySelector('#result');
+
+    dispayDiv.textContent = firstNumber - secondNumber;
 }
