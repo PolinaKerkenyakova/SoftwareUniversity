ea44d385d07ff99832f3eefb9bc037bbb28e5518
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-02 15:49:39 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,38 @@
-function solve() {
-  //TODO...
+function solve(text, casingType) {
+
+  text = document.querySelector('#text').value;
+  casingType = document.querySelector('#naming-convention').value;
+  let resultField = document.querySelector('#result');
+
+  let output = '';
+  text = text.toLowerCase();
+
+  let words = text.split(' ');
+  if (casingType === 'Camel Case') {
+    words.forEach((word, i) => {
+      if (i !== 0) {
+        output += capitalize(word);
+      } else {
+        output += word
+      }
+    });
+
+  } else if (casingType === 'Pascal Case') {
+    words.forEach((word) => {
+      output += capitalize(word);
+    });
+
+  } else {
+    output = 'Error!';
+  }
+
+  function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  resultField.textContent += `${output}`
 }
+
+// solve("this is an example", "Camel Case");
+// solve("secOND eXamPLE", "Pascal Case");
+// solve("Invalid Input", "Another Case");
