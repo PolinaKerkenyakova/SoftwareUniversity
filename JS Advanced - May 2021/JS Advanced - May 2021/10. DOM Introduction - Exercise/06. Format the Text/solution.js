69af8d90aa92bf78dab1ed57762b27f7a578bbfb
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-03 10:25:01 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/10. DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-  //TODO
+  let textArea = document.querySelector('#input');
+  let text = textArea.value;
+  let outputField = document.querySelector('#output')
+
+  let sentances = text.split('.').map(x => x.trim()).filter(x => x.length > 0);
+  // sentances = sentances.map(x => x.trim());
+  // sentances = sentances.filter(x => x.length > 0);
+
+
+  for (let i = 0; i < sentances.length; i += 3) {
+    let arr = [];
+
+    for (let j = 0; j < 3; j++) {
+      if (sentances[j + i]) {
+        arr.push(sentances[j + i]);
+      }
+    }
+
+    let paragraph = arr.join('. ') + '.';
+    outputField.innerHTML += `<p>${paragraph}</p>`;
+  }
 }
