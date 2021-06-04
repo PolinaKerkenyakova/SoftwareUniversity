af64b8b49d6d2ba6d50f290cc8c54b9ba072bde2
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-04 12:48:31 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/08. Objects and Composition - Exercise/04. Heroic Inventory.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/08. Objects and Composition - Exercise/04. Heroic Inventory.js
@@ -1,8 +1,27 @@
-function heroicInventory() {
+function heroicInventory(inputArr) {
 
-    
+    let output = [];
+
+    inputArr.forEach(x => {
+        let [name, level, items] = x.split(' / ');
+        level = Number(level);
+
+        items = items ? items.split(', ') : [];
+
+        output.push({ name, level, items })
+    });
+
+    return JSON.stringify(output);
 }
 
-console.log(heroicInventory());
+console.log(heroicInventory(
+    [
+        'Isacc / 25 / Apple, GravityGun',
+        'Derek / 12 / BarrelVest, DestructionSword',
+        'Hes / 1 / Desolator, Sentinel, Antara'
+    ]
+));
 
-console.log(heroicInventory());
+console.log(heroicInventory(
+    ['Jake / 1000 / Gauss, HolidayGrenade']
+));
