3c1a3d20b98be13758f276e1a2d8ad6cc6dded79
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-07 11:51:31 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/11. DOM Manipulation and Events - Lab/03. Delete from Table/app.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/11. DOM Manipulation and Events - Lab/03. Delete from Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let emails = document.querySelectorAll('tbody tr td:nth-child(2)');
+    let inputField = document.querySelector('input[type=text]');
+    let inputValue = inputField.value;
+
+    let result = document.querySelector('#result');
+    for (let email of emails) {
+        if (email.textContent === inputValue) {
+            let tds = email.parentNode;
+            tds.parentNode.removeChild(tds);
+
+            result.textContent = 'Deleted.'
+            return
+        }
+    }
+
+    result.textContent = 'Not found.'
 }
