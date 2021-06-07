a008dfe725cfc0eb8a4c98a524832e61e529919e
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-07 13:09:21 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/11. DOM Manipulation and Events - Lab/05. Highlight Active/app.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/11. DOM Manipulation and Events - Lab/05. Highlight Active/app.js
@@ -1,3 +1,19 @@
 function focused() {
-    console.log('TODO:...');
+    let inputs = document.querySelectorAll('div input');
+
+    for (let input of inputs) {
+        input.addEventListener('focus', focus);
+        input.addEventListener('blur', blur);
+    }
+
+    function focus(e) {
+        let parent = e.target.parentNode;
+        console.log(parent);
+        parent.classList.add('focused');
+    }
+
+    function blur(e) {
+        let parent = e.target.parentNode;
+        parent.classList.remove('focused');
+    }
 }
