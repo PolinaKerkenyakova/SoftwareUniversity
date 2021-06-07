e70589431671748191ccf6cebb643950d41e6cd9
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-07 10:53:12 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/11. DOM Manipulation and Events - Lab/02. Add  Delete/app.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/11. DOM Manipulation and Events - Lab/02. Add  Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+    let itemsList = document.querySelector('#items');
+    let inputField = document.querySelector('#newItemText');
+    let inputValue = inputField.value;
+
+    if (inputValue.length === 0) { return };
+
+    let newLiItem = document.createElement('li');
+    newLiItem.textContent = inputValue;
+
+    let remove = document.createElement('a');
+    let textDelete = document.createTextNode('[Delete]');
+    remove.appendChild(textDelete);
+    remove.setAttribute('href', '#');
+    newLiItem.appendChild(remove);
+
+    remove.addEventListener('click', deleteElement);
+
+    itemsList.appendChild(newLiItem);
+
+    inputField.value = '';
+
+    function deleteElement() {
+        newLiItem.remove();
+    }
 }
