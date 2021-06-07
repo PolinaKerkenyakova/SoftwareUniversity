7041ade7c4c78b773e6ae5afd8b0294abeaa98f5
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-07 16:27:40 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/11. DOM Manipulation and Events - Lab/06. Dynamic Validation/app.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/11. DOM Manipulation and Events - Lab/06. Dynamic Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    let regex = /[a-z]+@[a-z]+.[a-z]+/g;
+
+    let inputField = document.querySelector('#email');
+
+    inputField.addEventListener('change', checkForError);
+
+    function checkForError(event) {
+        if (regex.test(event.target.value)) {
+            event.target.classList.remove('error');
+            return;
+        } else {
+            event.target.classList.add('error');
+        }
+    }
 }
--- JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+
+   let contentDiv = document.querySelector('#content');
+   for (let word of words) {
+      let newDiv = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = word;
+      p.style.display = 'none';
+      newDiv.appendChild(p);
+      newDiv.addEventListener('click', showParagraph);
+      contentDiv.appendChild(newDiv);
+   }
+
+   function showParagraph(event) {
+      let divParent = event.target.children;
+      divParent[0].style.display = 'block';
+   }
 }
