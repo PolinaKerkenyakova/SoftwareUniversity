14df869a0209948dcd79d2c0a68f4888840293aa
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-08 11:45:46 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/03. Locked Profile/app.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/03. Locked Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let btn = Array.from(document.querySelectorAll('button'));
+
+    btn.forEach(x => x.addEventListener('click', showHide));
+
+    function showHide(event) {
+        let button = event.target;
+        let profile = button.parentElement;
+        let moreInfo = profile.querySelectorAll('div')[0];
+        let lockStatus = profile.querySelector('input[type=radio]:checked').value;
+
+        if (lockStatus === 'unlock') {
+            if (button.textContent === 'Show more') {
+                moreInfo.style.display = 'inline-block';
+                button.textContent = 'Hide it';
+            } else if (button.textContent === 'Hide it') {
+                moreInfo.style.display = 'none';
+                button.textContent = 'Show more';
+            }
+        }
+    }
 }
