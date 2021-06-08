d877a5c5b6d5136e92fc1704f2a0b206982ffe5d
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-08 11:11:23 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/02. Time Converter/solution.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/02. Time Converter/solution.js
@@ -1,4 +1,40 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let daysBtn = document.querySelector('#daysBtn');
+    let hoursBtn = document.querySelector('#hoursBtn');
+    let minutesBtn = document.querySelector('#minutesBtn');
+    let secondsBtn = document.querySelector('#secondsBtn');
+
+    let inputDays = document.querySelector('#days');
+    let inputHours = document.querySelector('#hours');
+    let inputMinutes = document.querySelector('#minutes');
+    let inputSeconds = document.querySelector('#seconds');
+
+    daysBtn.addEventListener('click', function(event) {
+        let days = Number(inputDays.value);
+        inputHours.value = days * 24;
+        inputMinutes.value = days * 1440;
+        inputSeconds.value = days * 86400;
+    });
+
+    hoursBtn.addEventListener('click', function(event) {
+        let hours = Number(inputHours.value);
+        inputDays.value = hours / 24;
+        inputMinutes.value = hours * 60;
+        inputSeconds.value = hours * 60 * 60;
+    });
+
+    minutesBtn.addEventListener('click', function(event) {
+        let minutes = Number(inputMinutes.value);
+        inputDays.value = minutes / 1440;
+        inputHours.value = minutes / 60;
+        inputSeconds.value = minutes * 60;
+    });
+
+    secondsBtn.addEventListener('click', function(event) {
+        let seconds = Number(inputSeconds.value);
+        inputDays.value = seconds / 86400;
+        inputHours.value = seconds / 60 / 60;
+        inputMinutes.value = seconds / 60;
+    });
 }
