e747c8b242c55e7958c679dac569e8b02887b603
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-15 16:55:34 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/05. Encode and Decode Messages/app.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/05. Encode and Decode Messages/app.js
@@ -1,3 +1,43 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    // Encode message and sent it functionality
+    let encodeArea = Array.from(document.querySelectorAll('textarea'))[0];
+    let encodeBtn = Array.from(document.querySelectorAll('button'))[0];
+
+    encodeBtn.addEventListener('click', onEncoding);
+
+
+    // Decode messge and read it functionality
+    let decodeArea = Array.from(document.querySelectorAll('textarea'))[1]
+    let decodeBtn = Array.from(document.querySelectorAll('button'))[1];
+
+    decodeBtn.addEventListener('click', onDecoding);
+
+    function onEncoding(e) {
+        let text = encodeArea.value.split('');
+        let asciiPlusOne = [];
+        text.map(char => {
+            let code = char.charCodeAt(0);
+            asciiPlusOne.push(code + 1)
+        });
+
+        let modifiedText = asciiPlusOne.map(x => String.fromCharCode(x));
+
+        encodeArea.value = '';
+        decodeArea.value = modifiedText.join('');
+
+    }
+
+    function onDecoding(e) {
+        let text = decodeArea.value.split('');
+        let asciiMinusOne = [];
+
+        text.map(char => {
+            let code = char.charCodeAt(0);
+            asciiMinusOne.push(code - 1);
+        });
+
+        let modifiedText = asciiMinusOne.map(x => String.fromCharCode(x));
+        decodeArea.value = modifiedText.join('');
+    }
 }
