10a95d6674b80d338a259355d0aae0ec651daa35
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-16 13:42:52 +0300

    Solution Update

--- JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/07. Distance Converter/distanceConverter.js
+++ JS Advanced - May 2021/JS Advanced - May 2021/12. DOM Manipulation and Events - Exercise/07. Distance Converter/distanceConverter.js
@@ -1,3 +1,16 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+
+    let fromInput = document.querySelector('#inputDistance');
+    let fromUnit = document.querySelector('#inputUnits');
+
+    let toInput = document.querySelector('#outputDistance');
+    let toUnit = document.querySelector('#outputUnits');
+
+    let convertBtn = document.querySelector('#convert');
+
+    convertBtn.addEventListener('click', onClick);
+
+    function onClick() {
+        
+    }
 }
