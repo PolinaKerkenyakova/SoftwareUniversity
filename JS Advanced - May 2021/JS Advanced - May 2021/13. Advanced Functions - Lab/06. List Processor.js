8226e83d3a9eaeb52c0799c59771464409e63361
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-29 15:55:43 +0300

    Initial commit

---
+++ JS Advanced - May 2021/JS Advanced - May 2021/13. Advanced Functions - Lab/06. List Processor.js
@@ -0,0 +1,18 @@
+function listProcessor(collection) {
+    let result = [];
+
+    let commands = {
+        add: str => result.push(str),
+        remove: str => result = result.filter(x => x !== str),
+        print: () => console.log(result.join(', '))
+    }
+
+    collection.forEach(line => {
+        let [command, word] = line.split(' ');
+        commands[command](word);
+    });
+}
+
+listProcessor(['add hello', 'add again', 'remove hello', 'add again', 'print']);
+
+listProcessor(['add pesho', 'add george', 'add peter', 'remove peter', 'print']);
