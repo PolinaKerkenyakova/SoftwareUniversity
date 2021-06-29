490c72d8a40ca704160e68a707317071aa80aa99
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-06-29 15:17:00 +0300

    Solution Update

---
+++ JS Advanced - May 2021/JS Advanced - May 2021/13. Advanced Functions - Lab/04. Filter Employees.js
@@ -0,0 +1,85 @@
+function filterEmployees(data, criteria) {
+
+    let [property, value] = criteria.split('-');
+
+    let index = 0;
+
+    JSON.parse(data).forEach(person => searchByCriteria.call(person));
+
+    function searchByCriteria() {
+        if (this[property] === value || criteria === 'all') {
+            return console.log(`${index++}. ${this.first_name} ${this.last_name} - ${this.email}`);
+        }
+    }
+}
+
+filterEmployees(`[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`,
+    'all');
+
+filterEmployees(`[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`,
+    'gender-Female');
+
+filterEmployees(`[{
+    "id": "1",
+    "first_name": "Kaylee",
+    "last_name": "Johnson",
+    "email": "k0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Johnson",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  }, {
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }, {
+    "id": "4",
+    "first_name": "Evanne",
+    "last_name": "Johnson",
+    "email": "ev2@hostgator.com",
+    "gender": "Male"
+  }]`,
+    'last_name-Johnson');
