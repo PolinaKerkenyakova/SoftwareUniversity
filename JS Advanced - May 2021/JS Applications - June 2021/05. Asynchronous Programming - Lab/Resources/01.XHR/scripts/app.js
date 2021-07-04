4b6a452b5c1170b8a55bd0ddfdafc261a1e678b9
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-07-04 14:00:23 +0300

    Solution Update

--- JS Advanced - May 2021/JS Applications - June 2021/05. Asynchronous Programming - Lab/Resources/01.XHR/scripts/app.js
+++ JS Advanced - May 2021/JS Applications - June 2021/05. Asynchronous Programming - Lab/Resources/01.XHR/scripts/app.js
@@ -1,3 +1,12 @@
 function loadRepos() {
-   console.log("TODO...");
+   const httpRequest = new XMLHttpRequest();
+
+   httpRequest.addEventListener('readystatechange', () => {
+      if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+         document.querySelector('#res').textContent = httpRequest.responseText;
+      }
+   });
+
+   httpRequest.open('GET', 'https://api.github.com/users/testnakov/repos');
+   httpRequest.send();
 }
