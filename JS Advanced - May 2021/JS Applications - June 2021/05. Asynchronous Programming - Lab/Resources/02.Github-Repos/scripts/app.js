18df76a9de68c28ea327847c4f3033fde90f703e
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-07-04 14:37:31 +0300

    Solution Update

--- JS Advanced - May 2021/JS Applications - June 2021/05. Asynchronous Programming - Lab/Resources/02.Github-Repos/scripts/app.js
+++ JS Advanced - May 2021/JS Applications - June 2021/05. Asynchronous Programming - Lab/Resources/02.Github-Repos/scripts/app.js
@@ -1,3 +1,30 @@
 function loadRepos() {
-	console.log("TODO...");
+	let username = document.querySelector('#username').value;
+	return fetch(`https://api.github.com/users/${username}/repos`)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			// console.log(data)
+			showRepos(data);
+		})
+		.catch(err => {
+			console.log('Err', err)
+		})
+}
+
+const showRepos = (data) => {
+	let ulElement = document.querySelector('#repos');
+	ulElement.innerHTML = '';
+
+	data.forEach(repo => {
+		let liElement = document.createElement('li');
+
+		let a = document.createElement('a');
+		a.href = repo.html_url;
+		a.textContent = repo.full_name;
+
+		liElement.appendChild(a);
+		ulElement.appendChild(liElement);
+	});
 }
