fa72abf275973e3e786373f25ac1438c5678150e
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-07-05 10:06:22 +0300

    Solution Update

--- JS Advanced - May 2021/JS Applications - June 2021/05. Asynchronous Programming - Lab/Resources/03.Gighub-Commits/app.js
+++ JS Advanced - May 2021/JS Applications - June 2021/05. Asynchronous Programming - Lab/Resources/03.Gighub-Commits/app.js
@@ -1,4 +1,35 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.querySelector('#username').value;
+    let repo = document.querySelector('#repo').value;
+    return fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.status)
+            }
+        })
+        .then(data => {
+            showCommits(data);
+        })
+        .catch(err => {
+            showError(err.message);
+        });
+}
+
+const showError = (err) => {
+    liCreator(`${err} (Not Found)`);
+}
+
+const showCommits = (data) => {
+    data.forEach(c => {
+        liCreator(`${c.commit.author.name}: ${c.commit.message}`)
+    });
+}
+
+const liCreator = (input) => {
+    let ul = document.querySelector('#commits');
+    let li = document.createElement('li');
+    li.textContent = input;
+    ul.appendChild(li);
 }
