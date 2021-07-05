21f01d46ad8407d01da26101fda27270ec1ac3d0
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-07-05 14:14:05 +0300

    Solution Update

--- JS Advanced - May 2021/JS Applications - June 2021/06. Asynchronous Programming - Exercise/01. Bus-Stop/app.js
+++ JS Advanced - May 2021/JS Applications - June 2021/06. Asynchronous Programming - Exercise/01. Bus-Stop/app.js
@@ -1,3 +1,23 @@
 function getInfo() {
-    console.log("TODO...");
+    document.querySelector('#buses').innerHTML = '';
+    let stopId = document.querySelector('#stopId').value;
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+        .then(response => response.json())
+        .then(data => {
+            busName(data.name);
+            addLi(data.buses);
+        })
+        .catch(error => {
+            busName('Error');
+        });
+}
+
+const busName = (input) => document.querySelector('#stopName').textContent = input;
+
+const addLi = (busesObj) => {
+    Object.entries(busesObj).map(([bus, minutes]) => {
+        const li = document.createElement('li');
+        li.textContent = `Bus ${bus} arrives in ${minutes} minutes`;
+        document.querySelector('#buses').appendChild(li);
+    });
 }
