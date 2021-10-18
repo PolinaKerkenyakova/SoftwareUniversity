1e2d9d65fbca7e2d8aaf74ddaac5fc0802d2fa75
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-10-18 10:48:10 +0300

    Updated validation on the House mongoose model

--- JS Web/JS Back-end/15. Exam Preparation/Real estate agency/real-estate-agency/models/Housing.js
+++ JS Web/JS Back-end/15. Exam Preparation/Real estate agency/real-estate-agency/models/Housing.js
@@ -2,15 +2,14 @@ const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
     name: { type: String, required: true, minlength: [6, 'Name should be at least 6 chars long'] },
-    type: { type: String, enum: ['Apartment', 'Villa', 'House'], required: true },
+    type: { type: String, enum: { values: ['Apartment', 'Villa', 'House'], message: 'Property type should be Apartment, Villa or House' }, required: true },
     year: { type: Number, required: true, min: [1850, 'Year should be after 1850'], max: [2021, 'Year should be before 2021'] },
     city: { type: String, required: true, minlength: [4, 'City should be at least 4 chars long'] },
-    homeImage: { type: String, required: true },
+    homeImage: { type: String, required: [true, 'Image of the property is required'] },
     description: { type: String, required: true, maxlength: [60, 'Description length can not exceed 60 chars'] },
-    availablePieces: { type: Number, required: true, min: [0, 'Available pieces should be a positive number'], max: [10, 'Available pieces can not exceed 10'] },
-    rentedHome: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
+    availablePieces: { type: Number, required: [true, 'Available pieces can be from 0 to 10'], min: 0, max: 10 },
+    homeRentedBy: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
     owner: { type: Schema.Types.ObjectId, ref: 'User' }
-
 });
 
 module.exports = model('Housing', schema)
