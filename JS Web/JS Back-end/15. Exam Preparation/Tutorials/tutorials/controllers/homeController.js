0df22eef3f65e49557208de9c43e763350cca69c
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-10-21 12:04:53 +0300

    Added date format to the main course card

--- JS Web/JS Back-end/15. Exam Preparation/Tutorials/tutorials/controllers/homeController.js
+++ JS Web/JS Back-end/15. Exam Preparation/Tutorials/tutorials/controllers/homeController.js
@@ -5,6 +5,11 @@ router.get('/', async (req, res) => {
     if (req.user) {
         const courses = await req.storage.getAllCourses();
 
+        courses.forEach(c => {
+
+            let date = c.createdAt.toString().split('GMT')
+            c.createdAt = date[0].trim();
+        });
         res.render('home/user', { courses });
     } else {
         const courses = await req.storage.getTopThreeCourses();
