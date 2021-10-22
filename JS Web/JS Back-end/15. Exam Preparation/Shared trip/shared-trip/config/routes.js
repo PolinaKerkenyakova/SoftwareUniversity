b976425958283c98db821502bee102f0726f433c
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-10-22 14:50:25 +0300

    Update routes

--- JS Web/JS Back-end/15. Exam Preparation/Shared trip/shared-trip/config/routes.js
+++ JS Web/JS Back-end/15. Exam Preparation/Shared trip/shared-trip/config/routes.js
@@ -1,5 +1,11 @@
+const homeController = require('../controllers/homeController.js');
 const authController = require('../controllers/authController.js');
+const tripController = require('../controllers/tripController.js');
+const errorController = require('../controllers/errorController.js');
 
 module.exports = (app) => {
-    app.use('/auth', authController)
+    app.use('/', homeController);
+    app.use('/auth', authController);
+    app.use('/trip', tripController);
+    app.use('*', errorController);
 };
