95993993ad378be5e5c376ce4f0954f97bea3f23
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-10-22 14:51:10 +0300

    Delete unnecessary comments

--- JS Web/JS Back-end/15. Exam Preparation/Shared trip/shared-trip/index.js
+++ JS Web/JS Back-end/15. Exam Preparation/Shared trip/shared-trip/index.js
@@ -5,8 +5,6 @@ const databaseConfig = require('./config/database.js');
 const expressConfig = require('./config/express.js');
 const routesConfig = require('./config/routes.js');
 
-// const userService = require('./services/user.js');
-// const authMiddleware = require('./middlewares/auth.js');
 
 start();
 
@@ -18,40 +16,7 @@ async function start() {
     routesConfig(app)
 
     app.listen(PORT, () => {
-        // testAuth();
+
         console.log(`App started at http://localhost:${PORT}`)
     });
 };
-
-
-// async function testAuth() {
-
-//     // try {
-//     //     // const result = await userService.createUser('Peter', 'asdasd');
-//     //     // console.log(result);
-
-//     //     const user = await userService.getUserByUsername('peter');
-//     //     console.log(user);
-
-//     // } catch (err) {
-//     //     console.log('Error:', err.message);
-//     // }
-
-//     const reqMock = {};
-//     const resMock = {
-//         cookie() {
-//             console.log('Set cookie', arguments);
-//         }
-//     };
-//     const nextMock = () => { };
-
-//     try {
-//         const auth = authMiddleware();
-//         auth(reqMock, resMock, nextMock);
-
-//         await reqMock.auth.login('john', 'asdfgh');
-
-//     } catch (err) {
-//         console.log('Error:', err.message);
-//     }
-// } 
