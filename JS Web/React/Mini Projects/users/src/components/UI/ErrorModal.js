dcf8c5f3008d9cf3f6ef7d61d02e1dde19c1cfec
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-08 14:18:17 +0200

    Create portal for the error modal

--- JS Web/React/Mini Projects/users/src/components/UI/ErrorModal.js
+++ JS Web/React/Mini Projects/users/src/components/UI/ErrorModal.js
@@ -1,25 +1,38 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
 import './ErrorModal.css';
 
 import Card from './Card';
 import Button from './Button';
 
+const Backdrop = props => {
+    return <div className="backdrop" onClick={props.onConfirm}></div>;
+}
+
+const Modal = props => {
+    return (
+        <Card className="modal">
+            <header className="header">
+                <h2>{props.title}</h2>
+            </header>
+            <div className="content">
+                <p>{props.message}</p>
+            </div>
+            <footer className="actions">
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    )
+}
+
 const ErrorModal = (props) => {
 
     return (
-        <>
-            <div className="backdrop" onClick={props.onConfirm}></div>
-            <Card className="modal">
-                <header className="header">
-                    <h2>{props.title}</h2>
-                </header>
-                <div className="content">
-                    <p>{props.message}</p>
-                </div>
-                <footer className="actions">
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
-        </>
+        <React.Fragment>
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<Modal title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('overlay-root'))}
+        </React.Fragment>
     )
 }
 
