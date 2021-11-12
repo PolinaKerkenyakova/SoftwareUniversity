be6de2aa7bcb142c2ed0fa32fb2845c53de7d2a2
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-12 11:47:27 +0200

    Add item to the cart

--- JS Web/React/Mini Projects/food-order-app/src/components/Meals/MealItem/MealItem.js
+++ JS Web/React/Mini Projects/food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,24 @@
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
+
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
 
+
 const MealItem = props => {
 
-const price = `$${props.price.toFixed(2)}`;
+    const cartCtx = useContext(CartContext)
+    const price = `$${props.price.toFixed(2)}`;
+
+    const addToCardHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
+
 
     return (
         <li className={classes.meal}>
@@ -13,7 +28,7 @@ const price = `$${props.price.toFixed(2)}`;
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm onAddToCard={addToCardHandler} />
             </div>
         </li>
     )
--- JS Web/React/Mini Projects/food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ JS Web/React/Mini Projects/food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,18 +1,38 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
+    const [isAmountValid, setIsAmountValid] = useState(true);
+    const amountInputRef = useRef();
+    const submitHadler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+
+        if (enteredAmount.trim().length === 0 || Number(enteredAmount) < 1 || Number(enteredAmount) > 5) {
+            setIsAmountValid(false);
+            return;
+        }
+
+        props.onAddToCard(Number(enteredAmount));
+    }
+
     return (
-        <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: 'amount_' + props.id,
-                type: 'number',
-                min: '1',
-                max: '5',
-                step: '1',
-                defaultValue: '1'
-            }} />
+        <form className={classes.form} onSubmit={submitHadler}>
+            <Input
+                ref={amountInputRef}
+                label="Amount"
+                input={{
+                    id: 'amount_' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1'
+                }} />
             <button>+ Add</button>
+            {!isAmountValid && <p>Plaease enter a valid amount (1-5).</p>}
         </form>
     )
 }
