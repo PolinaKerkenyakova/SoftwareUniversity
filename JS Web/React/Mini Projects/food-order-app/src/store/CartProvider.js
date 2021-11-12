c066d73f83cecebf9544e54f62d85442c1b4f52c
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-12 13:24:16 +0200

    Update card on adding new or already existing item

--- JS Web/React/Mini Projects/food-order-app/src/store/CartProvider.js
+++ JS Web/React/Mini Projects/food-order-app/src/store/CartProvider.js
@@ -10,8 +10,27 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
 
     if (action.type === 'ADD_ITEM') {
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.amount * action.item.price;
+
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amout + action.state.amount
+            }
+
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item)
+        }
+
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
@@ -26,11 +45,11 @@ const CartProvider = props => {
     const [cartState, dispachCartAction] = useReducer(cartReducer, defaultCartState);
 
     const addItemToCartHandler = (item) => {
-        dispachCartAction({type: 'ADD_ITEM', item: item})
+        dispachCartAction({ type: 'ADD_ITEM', item: item })
     };
 
     const removeItemFromCartHandler = (id) => {
-        dispachCartAction({type: 'REMOVE_ITEM', id: id})
+        dispachCartAction({ type: 'REMOVE_ITEM', id: id })
     };
 
     const cartContext = {
@@ -43,7 +62,7 @@ const CartProvider = props => {
     return (
         <CartContext.Provider value={cartContext}>
             {props.children}
-        </CartContext.Provider> 
+        </CartContext.Provider>
     )
 }
 
