eacecb95459f3a5f8c4862d79040e596916b3dcb
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-13 12:13:47 +0200

    Add dynamic loading

--- JS Web/React/Mini Projects/movies/src/App.js
+++ JS Web/React/Mini Projects/movies/src/App.js
@@ -4,13 +4,16 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [isLoadingData, setIsLoadingData] = useState(false);
 
   const fetchMoviesHandler = () => {
+    setIsLoadingData(true);
     fetch('https://swapi.dev/api/films/')
       .then(body => body.json())
       .then(result => {
-          setMovies(result.results)
+        setMovies(result.results)
+        setIsLoadingData(false);
       });
   }
 
@@ -20,7 +23,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoadingData && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoadingData && movies.length === 0 && <p>Found no movies!</p>}
+        {isLoadingData && <p>Loading</p>}
       </section>
     </React.Fragment>
   );
