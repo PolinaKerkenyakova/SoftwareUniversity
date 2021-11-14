7a6ee3bc4fd842c8370b496e972d706dde20d09b
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-14 13:40:13 +0200

    Add UI validation on user input

--- JS Web/React/Mini Projects/forms/src/components/SimpleInput.js
+++ JS Web/React/Mini Projects/forms/src/components/SimpleInput.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameFieldTouched, setEnteredNameFieldTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim().length !== 0;
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameFieldTouched;
+
+  const enteredNameHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const fieldTouchedHandler = (event) => {
+    setEnteredNameFieldTouched(true);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameFieldTouched(true);
+
+    setEnteredName('');
+    setEnteredNameFieldTouched(false);
+
+  }
+
+
+  const formClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={submitHandler}>
+      <div className={formClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={enteredNameHandler} onBlur={fieldTouchedHandler} value={enteredName} />
+        {nameInputIsInvalid && <p className='error-text'>Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
