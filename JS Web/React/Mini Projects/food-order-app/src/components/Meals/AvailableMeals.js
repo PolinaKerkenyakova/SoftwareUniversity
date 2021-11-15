cc12c8cebb7e70d5b29e548d8fa5a36fce2e98d9
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-11-15 16:08:22 +0200

    Add UI info for loading, no available meals or render available meals

--- JS Web/React/Mini Projects/food-order-app/src/components/Meals/AvailableMeals.js
+++ JS Web/React/Mini Projects/food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,41 +1,62 @@
+import { useEffect, useState } from 'react/cjs/react.development';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Loader from '../UI/Loader'
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(m => <MealItem key={m.id} id={m.id} name={m.name} description={m.description} price={m.price} />)
+
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState
+    (true)
+  const [hasError, setHasError] = useState(null)
+    ;
+
+  const fetchData = async () => {
+
+    const response = await fetch('https://food-order-app-dcf28-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const result = await response.json();
+    const meals = [];
+
+    Object.entries(result).map(([id, info]) => {
+      return meals.push({ id, ...info });
+    });
+
+    setMeals(meals)
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+
+    fetchData().catch(error => {
+      setIsLoading(false);
+      setHasError(error.message)
+    });
+  }, []);
+
+
+  const mealsList = meals.map(m => <MealItem key={m.id} id={m.id} name={m.name} description={m.description} price={m.price} />);
+
+let content = mealsList;
+
+  if (isLoading) {
+   content = <Loader />
+  }
+
+  if (hasError) {
+    content =  <p>{hasError}</p>;
+  }
+
   return (
     <section className={classes.meals}>
       <Card>
         <ul>
-          {mealsList}
+          {content}
         </ul>
       </Card>
     </section>
